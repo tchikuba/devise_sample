529957f28cf3858bf51b2592c9c60e57dcf2d3b1
tchikuba <chikuba.tsutomu@gmail.com>
2018-05-30 02:19:39 +0900

    Userモデルにコントローラーから呼び出される処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:twitter, :facebook]
+
+  class << self
+    def find_or_create_for_oauth(auth)
+      find_or_create_by!(email: auth.info.email) do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.email = auth.info.email
+        password = Devise.friendly_token[0, 20]
+        logger.debug password
+        user.password = password
+      end
+    end
+
+    def new_with_session(params, session)
+      if user_attributes = session['devise.user_attributes']
+        new(user_attributes) { |user| user.attributes = params }
+      else
+        super
+      end
+    end
+  end
 end
